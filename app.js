a9a01f0cf5456728300f7b8fbfdb32ad05fd5e68
Sahil Tiwari <sahiltiwari.9898@gmail.com>
2021-03-26 22:40:34 +0530

    hmm

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const app = express();
 var bse = api.BSE;
 var nse = api.NSE;
 
-app.get("/", (req, res)=>{
-    res.send("Home");
+app.get("/:symbol", (req, res)=>{
+    nse.getQuoteInfo(req.params.symbol).then(
+        function(price){
+                const x = price.data.data[0];
+                if(x.buyPrice1==='-'){
+                    res.send(x.lastPrice);
+                }else{
+                    res.send(x.buyPrice1);
+                }
+        });
 });
 
 app.get("/getPrice/:symbol", (req, res)=>{
