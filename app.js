ed886521eb956824d69a4878737a87b8bfd84d8b
Sahil Tiwari <60009481+sahil-9898@users.noreply.github.com>
2021-06-27 11:00:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var nse = api.NSE;
 
 app.get("/", (req, res) => {
   res.send(
-    "Daddy Sahil ne api banayi hai , go to https://nseapi.herokuapp.com/symbol_name to get stock price"
+    "https://nseapi.herokuapp.com/symbol_name to get stock price"
   );
 });
 app.get("/:symbol", (req, res) => {
