0251d5e85fde3345724f58ccf35494a61f33791d
Sahil Tiwari <sahiltiwari.9898@gmail.com>
2021-07-28 14:29:01 +0530

    add websocket connection

---
+++ ws.js
@@ -0,0 +1,24 @@
+var WebSocketClient = require("websocket").client;
+
+var client = new WebSocketClient();
+
+client.on("connectFailed", function (error) {
+  console.log("Connect Error: " + error.toString());
+});
+
+client.on("connect", function (connection) {
+  console.log("WebSocket Client Connected");
+  connection.on("error", function (error) {
+    console.log("Connection Error: " + error.toString());
+  });
+  connection.on("close", (connection) => {
+    console.log("connection closed");
+  });
+  connection.send('{"subscribe":["IRCTC.NS"]}');
+
+  connection.on("message", function (message) {
+    console.log(message);
+  });
+});
+
+client.connect("wss://streamer.finance.yahoo.com/", "echo-protocol");
